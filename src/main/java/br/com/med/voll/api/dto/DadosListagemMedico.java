ac625ffdf0b17d3baa2a7b90f757d9f85befa306
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-20 12:52:32 -0300

    feat(dto): adiciona nova classe DadosListagemMedico com construtor padrão

--- src/main/java/br/com/med/voll/api/dto/DadosListagemMedico.java
+++ src/main/java/br/com/med/voll/api/dto/DadosListagemMedico.java
@@ -1,4 +1,18 @@
 package br.com.med.voll.api.dto;
 
-public record DadosListagemMedico() {
+import br.com.med.voll.api.enums.Especialidade;
+import br.com.med.voll.api.model.Medico;
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+
+    public DadosListagemMedico(Medico medico) {
+        this(medico.getName(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
 }
