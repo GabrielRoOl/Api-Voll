a1bd518b67da7d4b276e7c1c633838f018e6cf77
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-20 14:15:45 -0300

    feat(repositories): corrige annotation

--- src/main/java/br/com/med/voll/api/repositories/PacienteRepository.java
+++ src/main/java/br/com/med/voll/api/repositories/PacienteRepository.java
@@ -2,6 +2,8 @@ package br.com.med.voll.api.repositories;
 
 import br.com.med.voll.api.model.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
