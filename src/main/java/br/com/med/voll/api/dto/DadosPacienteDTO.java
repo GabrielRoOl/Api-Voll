614ebae911fb1d25969b697541b6943b242dde2c
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-20 13:54:42 -0300

    feat(dtos): adiciona record DadosPacienteDTO

--- src/main/java/br/com/med/voll/api/dto/DadosPacienteDTO.java
+++ src/main/java/br/com/med/voll/api/dto/DadosPacienteDTO.java
@@ -1,12 +1,14 @@
 package br.com.med.voll.api.dto;
 
-import br.com.med.voll.api.model.Endereco;
+import jakarta.persistence.Column;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record DadosPacienteDTO(
         @NotBlank String nome,
         @NotBlank String cpf,
         @NotBlank String telefone,
-        @NotBlank Endereco endereco
+        @NotNull @Valid DadosEndereco endereco
         ) {
 }
