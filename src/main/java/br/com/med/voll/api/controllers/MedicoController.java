b438c972d7b95c448483017b97fb8b422fdfef55
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-20 12:58:38 -0300

    feat(controllers): adiciona endpoint Get listar paginado

--- src/main/java/br/com/med/voll/api/controllers/MedicoController.java
+++ src/main/java/br/com/med/voll/api/controllers/MedicoController.java
@@ -1,15 +1,18 @@
 package br.com.med.voll.api.controllers;
 
 import br.com.med.voll.api.dto.DadosCadastroMedico;
+import br.com.med.voll.api.dto.DadosListagemMedico;
 import br.com.med.voll.api.model.Medico;
 import br.com.med.voll.api.repositories.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,5 +25,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"name"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
