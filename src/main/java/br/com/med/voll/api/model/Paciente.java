52a1c388d43b4aa274cd0441bb07e8a8f2eb60ea
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-20 13:16:10 -0300

    feat(models): adiciona classe Paciente

---
+++ src/main/java/br/com/med/voll/api/model/Paciente.java
@@ -0,0 +1,60 @@
+package br.com.med.voll.api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Entity
+@Table(name = "tb_paciente")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(){}
+
+    public Paciente(String cpf, Endereco endereco, Long id, String nome) {
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
