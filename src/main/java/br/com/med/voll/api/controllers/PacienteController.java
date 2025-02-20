1beaf52a55fa9ffcc2be9a800aaf4c45a3a068b3
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-20 13:53:57 -0300

    feat(controllers): adiciona classe PacienteController com Post cadastrarPaciente

--- src/main/java/br/com/med/voll/api/controllers/PacienteController.java
+++ src/main/java/br/com/med/voll/api/controllers/PacienteController.java
@@ -1,4 +1,27 @@
 package br.com.med.voll.api.controllers;
 
+import br.com.med.voll.api.dto.DadosPacienteDTO;
+import br.com.med.voll.api.model.Paciente;
+import br.com.med.voll.api.repositories.PacienteRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("paciente")
 public class PacienteController {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrarPaciente(@RequestBody @Valid DadosPacienteDTO dados){
+        repository.save(new Paciente(dados));
+    }
+
 }
