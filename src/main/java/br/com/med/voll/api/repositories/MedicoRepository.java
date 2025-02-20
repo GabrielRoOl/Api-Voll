533951b5f92094987e4163b086aeed21831ec14e
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-20 12:49:37 -0300

    feat(repositories): adiciona nova consulta findTop10ByOrderByName

--- src/main/java/br/com/med/voll/api/repositories/MedicoRepository.java
+++ src/main/java/br/com/med/voll/api/repositories/MedicoRepository.java
@@ -1,9 +1,13 @@
 package br.com.med.voll.api.repositories;
 
 import br.com.med.voll.api.model.Medico;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    List<Medico> findTop10ByOrderByName(Pageable paginacao);
 }
