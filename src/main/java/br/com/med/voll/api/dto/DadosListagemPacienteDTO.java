0a007d2f28d85b733ccb6b4652b598b0ffc0ae42
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-24 09:37:29 -0300

    feat: adiciona parametro ao record DadosListagemPacienteDTO

--- src/main/java/br/com/med/voll/api/dto/DadosListagemPacienteDTO.java
+++ src/main/java/br/com/med/voll/api/dto/DadosListagemPacienteDTO.java
@@ -2,8 +2,8 @@ package br.com.med.voll.api.dto;
 
 import br.com.med.voll.api.model.Paciente;
 
-public record DadosListagemPacienteDTO(String nome, String cpf, String telefone) {
+public record DadosListagemPacienteDTO(Long id, String nome, String cpf, String telefone) {
     public DadosListagemPacienteDTO(Paciente paciente){
-        this(paciente.getNome(), paciente.getCpf(), paciente.getTelefone());
+        this(paciente.getId(), paciente.getNome(), paciente.getCpf(), paciente.getTelefone());
     }
 }
