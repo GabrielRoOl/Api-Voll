c9212fc30f6175eaa1f98c4a874b21a366590743
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-24 09:38:08 -0300

    feat: adiciona classe DadosAtualizaMedico

--- src/main/java/br/com/med/voll/api/dto/DadosAtualizaMedico.java
+++ src/main/java/br/com/med/voll/api/dto/DadosAtualizaMedico.java
@@ -1,4 +1,13 @@
 package br.com.med.voll.api.dto;
 
-public record DadosAtualizaMedico() {
+import br.com.med.voll.api.model.Endereco;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizaMedico(
+        @NotNull Long id,
+        String name,
+        String telefone,
+        DadosEndereco endereco) {
 }
