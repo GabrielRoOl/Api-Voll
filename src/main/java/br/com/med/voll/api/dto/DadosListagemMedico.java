d6c8cfbc051dbcd9388e045bd0c0c26556c614cd
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-24 09:36:43 -0300

    feat: adiciona parametro ao record DadosListagemPacienteDTO

--- src/main/java/br/com/med/voll/api/dto/DadosListagemMedico.java
+++ src/main/java/br/com/med/voll/api/dto/DadosListagemMedico.java
@@ -3,9 +3,9 @@ package br.com.med.voll.api.dto;
 import br.com.med.voll.api.enums.Especialidade;
 import br.com.med.voll.api.model.Medico;
 
-public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
 
     public DadosListagemMedico(Medico medico) {
-        this(medico.getName(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getName(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 }
