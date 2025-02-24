9c6db619f403046bb4f0de5f45a44dfb0793a50a
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-24 10:18:25 -0300

    refactor(api): altera endpoint de 'paciente' para 'pacientes'

--- src/main/java/br/com/med/voll/api/controllers/PacienteController.java
+++ src/main/java/br/com/med/voll/api/controllers/PacienteController.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("paciente")
+@RequestMapping("pacientes")
 public class PacienteController {
 
     @Autowired
