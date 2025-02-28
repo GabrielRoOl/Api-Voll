11326c1909891af60b9e8657475a4398f070f4a6
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-28 12:53:56 -0300

    feat: adiciona classe DadosDetalhamentoMedicoDTO

--- src/main/java/br/com/med/voll/api/dto/DadosDetalhamentoMedicoDTO.java
+++ src/main/java/br/com/med/voll/api/dto/DadosDetalhamentoMedicoDTO.java
@@ -1,4 +1,14 @@
 package br.com.med.voll.api.dto;
 
-public record DadosDetalhamentoMedicoDTO() {
+import br.com.med.voll.api.enums.Especialidade;
+import br.com.med.voll.api.model.Endereco;
+import br.com.med.voll.api.model.Medico;
+
+public record DadosDetalhamentoMedicoDTO(Long id, String name, String email, String telefone, String crm, Especialidade especialidade,
+                                         Endereco endereco) {
+
+    public DadosDetalhamentoMedicoDTO(Medico medico){
+        this(medico.getId(), medico.getName(), medico.getEmail(), medico.getTelefone(), medico.getCrm(),
+                medico.getEspecialidade(), medico.getEndereco());
+    }
 }
