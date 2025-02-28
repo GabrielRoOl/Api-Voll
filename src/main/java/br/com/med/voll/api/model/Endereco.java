bdb785b16eac1944f89acd88b263bfbd7afea51b
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-28 12:55:21 -0300

    feat: adiciona getters e setter à classe Endereco

--- src/main/java/br/com/med/voll/api/model/Endereco.java
+++ src/main/java/br/com/med/voll/api/model/Endereco.java
@@ -48,4 +48,60 @@ public class Endereco {
         if(endereco.uf() != null)
             this.uf = endereco.uf();
     }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
 }
