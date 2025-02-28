4ece6098be2fb69fc28dd9bdf36309ad7eecdef1
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-28 10:51:37 -0300

    feat: adiciona nova classe DTO DadosAtualizaPaciente

--- src/main/java/br/com/med/voll/api/dto/DadosAtualizaPaciente.java
+++ src/main/java/br/com/med/voll/api/dto/DadosAtualizaPaciente.java
@@ -1,4 +1,12 @@
 package br.com.med.voll.api.dto;
 
-public record DadosAtualizaPaciente() {
+import br.com.med.voll.api.model.Endereco;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizaPaciente(
+        @NotNull Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco
+) {
 }
