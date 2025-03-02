61885713beb78cbb0f800ac92e1e4f2c73a6c13b
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-03-02 13:33:10 -0300

    feat: adiciona ativação de atributo boolean

--- src/main/java/br/com/med/voll/api/model/Medico.java
+++ src/main/java/br/com/med/voll/api/model/Medico.java
@@ -107,22 +107,6 @@ public class Medico {
         this.telefone = telefone;
     }
 
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Medico medico = (Medico) o;
-        return id != null && Objects.equals(id, medico.id);
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
-
     public void atualizarInformacoes(@Valid DadosAtualizaMedico dados) {
         if(dados.name() != null)
             this.name = dados.name();
@@ -136,5 +120,9 @@ public class Medico {
 
     public void excluir() {
         this.ativo = false;
+    }
+
+    public void ativa() {
+        this.ativo = true;
     }
 }
