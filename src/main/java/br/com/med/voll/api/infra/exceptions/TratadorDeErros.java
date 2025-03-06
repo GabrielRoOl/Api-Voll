1ab425655ee9cf1c6fc2c874645b239d61e05436
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-03-06 10:48:05 -0300

    feat: trata erros 400 e 500

--- src/main/java/br/com/med/voll/api/infra/exceptions/TratadorDeErros.java
+++ src/main/java/br/com/med/voll/api/infra/exceptions/TratadorDeErros.java
@@ -1,11 +1,15 @@
 package br.com.med.voll.api.infra.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @RestControllerAdvice
 public class TratadorDeErros {
@@ -15,9 +19,21 @@ public class TratadorDeErros {
         return ResponseEntity.notFound().build();
     }
 
-    @ExceptionHandler(SQLException.class)
-    public ResponseEntity trataErro500(){
-        return ResponseEntity.badRequest().build();
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity trataErro400(MethodArgumentNotValidException ex){
+        var erros = ex.getBindingResult().getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity trataErro500(DataIntegrityViolationException  ex){
+        return ResponseEntity.badRequest().body("Erro: CRM ou e-mail já cadastrado!");
+    }
+
+    private record DadosErroValidacao(String campo, String msg){
+        public DadosErroValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
 }
