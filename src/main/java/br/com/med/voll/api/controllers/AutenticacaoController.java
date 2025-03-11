6b49e15f1f44d227579c4a04ee25efae8a19327b
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-03-10 21:53:13 -0300

    feat: Adiciona adiciona endpoint Post para login

--- src/main/java/br/com/med/voll/api/controllers/AutenticacaoController.java
+++ src/main/java/br/com/med/voll/api/controllers/AutenticacaoController.java
@@ -1,4 +1,25 @@
 package br.com.med.voll.api.controllers;
 
+import br.com.med.voll.api.dto.DadosAutenticacao;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/login")
 public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var autentication = manager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+    }
 }
