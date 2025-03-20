6b19d3687037435a918edd2a04369fb776e0e6ea
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-03-20 11:21:37 -0300

    refactor: melhora o tratamento de exceções

--- src/main/java/br/com/med/voll/api/infra/exceptions/TratadorDeErros.java
+++ src/main/java/br/com/med/voll/api/infra/exceptions/TratadorDeErros.java
@@ -2,37 +2,51 @@ package br.com.med.voll.api.infra.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.sql.SQLException;
-import java.util.List;
-
 @RestControllerAdvice
 public class TratadorDeErros {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity trataErro404(){
+    public ResponseEntity trataErro404() {
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity trataErro400(MethodArgumentNotValidException ex){
+    public ResponseEntity trataErro400(MethodArgumentNotValidException ex) {
         var erros = ex.getBindingResult().getFieldErrors();
 
         return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity tratarErro400(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity tratarErroBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity trataErro500(DataIntegrityViolationException  ex){
+    public ResponseEntity trataErro500(DataIntegrityViolationException ex) {
         return ResponseEntity.badRequest().body("Erro: CRM ou e-mail já cadastrado!");
     }
 
-    private record DadosErroValidacao(String campo, String msg){
-        public DadosErroValidacao(FieldError erro){
+    public ResponseEntity tratarErroAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+    
+    private record DadosErroValidacao(String campo, String msg) {
+        public DadosErroValidacao(FieldError erro) {
             this(erro.getField(), erro.getDefaultMessage());
         }
     }
