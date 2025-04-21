aabbcb8ace3d72705b745ddae4cf9f14c57e65f6
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-04-21 09:50:06 -0300

    feat: add JPQL for method escolherMedico

--- src/main/java/br/com/med/voll/api/repositories/MedicoRepository.java
+++ src/main/java/br/com/med/voll/api/repositories/MedicoRepository.java
@@ -1,11 +1,16 @@
 package br.com.med.voll.api.repositories;
 
 import br.com.med.voll.api.domain.model.medico.Medico;
+import br.com.med.voll.api.enums.Especialidade;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -14,4 +19,17 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
     List<Medico> findTop10ByOrderByName(Pageable paginacao);
 
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+
+    @Query("""
+            SELECT m FROM Medico m
+            WHERE m.ativo = true
+            AND m.especialidade = :especialidade
+            AND m.id not in(
+                SELECT c.medico.id FROM Consulta c
+                WHERE c.data = :data
+            )
+            ORDER BY rand()
+            LIMIT 1
+            """)
+    Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, @NotNull @Future LocalDateTime data);
 }
