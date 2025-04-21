d896b9199c1254a1471ef5a768b4c827bdde4b61
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-04-21 09:48:48 -0300

    refac: add method escolherMedico

--- src/main/java/br/com/med/voll/api/domain/model/consulta/AgendaDeConsultas.java
+++ src/main/java/br/com/med/voll/api/domain/model/consulta/AgendaDeConsultas.java
@@ -1,6 +1,8 @@
 package br.com.med.voll.api.domain.model.consulta;
 
+import br.com.med.voll.api.domain.model.medico.Medico;
 import br.com.med.voll.api.dto.DadosAgendamentoConsultaDTO;
+import br.com.med.voll.api.exceptions.ValidacaoException;
 import br.com.med.voll.api.repositories.ConsultaRepository;
 import br.com.med.voll.api.repositories.MedicoRepository;
 import br.com.med.voll.api.repositories.PacienteRepository;
@@ -22,13 +24,29 @@ public class AgendaDeConsultas {
 
     @Transactional
     public void agendar(DadosAgendamentoConsultaDTO dados) {
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Id do paciente informado não existe");
+        }
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("Id do médico informado não existe");
+        }
 
-
-        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+        var medico = escolherMedico(dados);
         var consulta = new Consulta(null, medico, paciente, dados.data());
 
         consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsultaDTO dados) {
+        if (dados.idMedico() != null) {
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+        if (dados.especialidade() == null) {
+            throw new ValidacaoException("Especialidade é obrigatória, caso médico não for escolhido");
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
     }
 
 }
