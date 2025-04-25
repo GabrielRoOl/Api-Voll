0d81854d54d7393b90761983e1ff49287daf36e2
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-04-25 09:57:22 -0300

    feat: adiciona validarPacienteSemConsulta

---
+++ src/main/java/br/com/med/voll/api/validacoes/ValidadorPacienteSemConsulta.java
@@ -0,0 +1,19 @@
+package br.com.med.voll.api.validacoes;
+
+import br.com.med.voll.api.dto.DadosAgendamentoConsultaDTO;
+import br.com.med.voll.api.exceptions.ValidacaoException;
+import br.com.med.voll.api.repositories.ConsultaRepository;
+
+public class ValidadorPacienteSemConsulta {
+
+    private ConsultaRepository repository;
+
+    public void validar(DadosAgendamentoConsultaDTO dados) {
+        var primeiroHorario = dados.data().withHour(7);
+        var ultimoHorario = dados.data().withHour(18);
+        var pacientePossuiOutraConsultaNoDia = repository.existsByPacienteIdAndDataBetween(dados.idPaciente(), dados.data());
+        if (pacientePossuiOutraConsultaNoDia) {
+            throw new ValidacaoException("Paciente já possui uma consulta agendada nesse dia");
+        }
+    }
+}
