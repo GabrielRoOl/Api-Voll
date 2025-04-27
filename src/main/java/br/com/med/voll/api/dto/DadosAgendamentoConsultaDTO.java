15af46d3ffbb55cf1e4fa2d914f15e62e80a9d57
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-04-27 12:32:17 -0300

    refactor: add annotation @JsonFormat(pattern = 'yyyy-MM-dd'T'HH:mm') for LocalDateTime

--- src/main/java/br/com/med/voll/api/dto/DadosAgendamentoConsultaDTO.java
+++ src/main/java/br/com/med/voll/api/dto/DadosAgendamentoConsultaDTO.java
@@ -2,6 +2,7 @@ package br.com.med.voll.api.dto;
 
 
 import br.com.med.voll.api.enums.Especialidade;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 
@@ -10,6 +11,6 @@ import java.time.LocalDateTime;
 public record DadosAgendamentoConsultaDTO(
         Long idMedico,
         @NotNull Long idPaciente,
-        @NotNull @Future LocalDateTime data,
+        @NotNull @Future @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime data,
         Especialidade especialidade) {
 }
