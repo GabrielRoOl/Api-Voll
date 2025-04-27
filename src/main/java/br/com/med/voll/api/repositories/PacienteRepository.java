b95248f6416521bfbc8fb95a20c86d595d089e35
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-04-27 12:39:23 -0300

    feature: add new query

--- src/main/java/br/com/med/voll/api/repositories/PacienteRepository.java
+++ src/main/java/br/com/med/voll/api/repositories/PacienteRepository.java
@@ -8,4 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     boolean findAtivoById(@NotNull Long aLong);
+
+    boolean existsByIdAndAtivoTrue(@NotNull Long id);
+
 }
