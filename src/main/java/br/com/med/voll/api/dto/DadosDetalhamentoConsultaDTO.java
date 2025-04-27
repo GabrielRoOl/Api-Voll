62b336ff044f923729b76aba06d1b8341390e9ca
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-04-27 12:33:02 -0300

    feature: add new constructor

--- src/main/java/br/com/med/voll/api/dto/DadosDetalhamentoConsultaDTO.java
+++ src/main/java/br/com/med/voll/api/dto/DadosDetalhamentoConsultaDTO.java
@@ -1,6 +1,11 @@
 package br.com.med.voll.api.dto;
 
+import br.com.med.voll.api.domain.model.consulta.Consulta;
+
 import java.time.LocalDateTime;
 
 public record DadosDetalhamentoConsultaDTO(Long id, Long idMedico, Long idPaciente, LocalDateTime data) {
+    public DadosDetalhamentoConsultaDTO(Consulta consulta) {
+        this(consulta.getId(), consulta.getMedico().getId(), consulta.getPaciente().getId(), consulta.getData());
+    }
 }
