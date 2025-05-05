22d1b39669d979c78da5d1fa6dd642e615e9e1b3
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-05-05 13:37:12 -0300

    refactor: update query

--- src/main/java/br/com/med/voll/api/repositories/MedicoRepository.java
+++ src/main/java/br/com/med/voll/api/repositories/MedicoRepository.java
@@ -28,7 +28,7 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
                 SELECT c.medico.id FROM Consulta c
                 WHERE c.data = :data
             )
-            ORDER BY rand()
+            ORDER BY RANDOM()
             LIMIT 1
             """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, @NotNull @Future LocalDateTime data);
